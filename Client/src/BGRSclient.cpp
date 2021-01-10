ba36eeda71d4dc1a8c60b553bebde39f7cac1b93
Meitar <meitargo@post.bgu.ac.il>
2021-01-10 13:28:45 +0200

    CHANGES IN readFromSrv and readToWrite

--- Client/src/BGRSclient.cpp
+++ Client/src/BGRSclient.cpp
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include "../include/readAndWrite.h"
-#include "../include/connectionHandler.h"
+//#include "../include/connectionHandler.h"
 
 #include <iostream>
 
@@ -8,35 +8,36 @@
 /**
 * This code assumes that the server replies the exact text the client sent it (as opposed to the practical session example)
 */
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
         return -1;
     }
+    //std::this_thread::sleep_until()
     std::string host = argv[1];
     short port = atoi(argv[2]);
-    bool terminate  = false;
+    bool terminate = false;
 
-    while(!terminate) {
-        ConnectionHandler connectionHandler(host, port);
-        readAndWrite readKey(connectionHandler, terminate);
-        std::thread th1(&readAndWrite::run, &readKey);
-        th1.join();
+    ConnectionHandler connectionHandler(host, port);
+    if (!connectionHandler.connect()) {
+        std::cerr << "Cannot connect " << endl;
+    }
 
+    readAndWrite readKey(connectionHandler, terminate);
+    std::thread th1(&readAndWrite::run, &readKey);
+
+    while (!terminate) {
 
         char bytes[2];//for the opCODE - BYTESTOSHORT
         connectionHandler.getBytes(bytes, 2);
         short opCode = connectionHandler.bytesToShort(bytes);
-        cout << "opCode:" << opCode << endl;
         char bytesOp[2];
 
         connectionHandler.getBytes(bytesOp, 2);
         short msgOp = connectionHandler.bytesToShort(bytesOp);
-        cout << "msgOp: " << msgOp << endl;
-
         if (opCode == 12) {
             string str = "";
-            if (msgOp == 6 || msgOp == 7 || msgOp == 8 || msgOp == 9 || msgOp==11) {
+            if (msgOp == 6 || msgOp == 7 || msgOp == 8 || msgOp == 9 || msgOp == 11) {
                 connectionHandler.getFrameAscii(str, '\0');
 
             }
@@ -46,14 +47,17 @@ int main (int argc, char *argv[]) {
             } else {
                 cout << "ACK " << msgOp << '\n' << str << endl;
             }
+
         } else if (opCode == 13) {
             cout << "ERR " << msgOp << endl;
         }
 
-        if (msgOp == 4) {
+        if (msgOp == 4 & opCode == 12) {
             terminate = true;
         }
     }
 
-    return 0;
+        exit(0);
+    //th1.join();
+    //return 0;
 }
